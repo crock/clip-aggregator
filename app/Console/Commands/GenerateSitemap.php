d2af3bd169ba8dee6c063f7657974e57cf43938a
Alex Crocker <croc122@gmail.com>
2019-03-14 19:32:58 -0400

    Made changes to sitemap generator

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -3,7 +3,11 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\SitemapIndex;
+use Spatie\Sitemap\Tags\Url;
+use App\Clip;
+use App\Game;
 
 class GenerateSitemap extends Command
 {
@@ -28,8 +32,31 @@ class GenerateSitemap extends Command
      */
     public function handle()
     {
-        // modify this to your own needs
-        SitemapGenerator::create(config('app.url'))
-            ->writeToFile(public_path('sitemap.xml'));
+		Sitemap::create()
+			->add(Url::create('/')->setPriority(1.0))
+			->add(Url::create('/upload')->setPriority(0.8)->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY))
+			->add(Url::create('/login')->setPriority(0.8)->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY))
+			->add(Url::create('/register')->setPriority(0.8)->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY))
+			->add(Url::create('/password/reset')->setPriority(0.4)->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY))
+			->writeToFile(public_path('pages_sitemap.xml'));
+
+		$games_sitemap = Sitemap::create();
+		Game::all()->each(function (Game $game) use ($games_sitemap) {
+			$games_sitemap->add(Url::create("/game/{$game->slug}")->setPriority(0.9)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
+		});
+		$games_sitemap->writeToFile(public_path('games_sitemap.xml'));
+
+		$clips_sitemap = Sitemap::create();
+		Clip::all()->each(function (Clip $clip) use ($clips_sitemap) {
+			$clips_sitemap->add(Url::create("/clip/{$clip->twitch_clip_id}")->setPriority(0.6)->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY));
+		});
+		$clips_sitemap->writeToFile(public_path('clips_sitemap.xml'));
+
+		SitemapIndex::create()
+			->add('/pages_sitemap.xml')
+			->add('/games_sitemap.xml')->setLastModificationDate(Carbon::today())
+			->add('/clips_sitemap.xml')->setLastModificationDate(Carbon::today())
+			->writeToFile(public_path('sitemap.xml'));
+
     }
 }
